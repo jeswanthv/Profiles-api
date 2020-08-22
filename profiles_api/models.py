4900c9a72ddab802b628ced030867295dc5e6e5e
jeswanthv <jeswanthv001@gmail.com>
2020-08-22 11:50:37 +0530

    Enable Django admin for user profile model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
 
@@ -8,30 +9,30 @@ class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
 
     def create_user(self, email, name, password=None):
-        """Create a New User Profile"""
+        """Create a new user profile"""
         if not email:
-            raise ValueError("User must have an email address")
+            raise ValueError('Users must have an email address')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
+        user = self.model(email=email, name=name,)
 
         user.set_password(password)
-        user.save(using=self.db)
+        user.save(using=self._db)
 
         return user
 
-        def create_superuser(self, email, name, password):
-            """Create a Superuser"""
-            user = self.creat_user(email, name, password)
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
 
-            user.is_superuser = True
-            user.is_staff = True
-            user.save(using = self._db)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
 
-            return user
+        return user
 
 
-class UserProfile(AbstractBaseUser):
+class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length = 255)
@@ -41,7 +42,7 @@ class UserProfile(AbstractBaseUser):
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = ['name']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
         return self.name
